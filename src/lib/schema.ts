a21a66bd5d72ea2d8d49717fa50cf8ad72f4829c
itsmikesharescode <mikejohneviota@gmail.com>
2024-05-01 09:52:10 +0800

    add schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,1 +1,6 @@
 import { z } from "zod";
+
+export const loginSchema = z.object({
+    email: z.string().email({ message: "Email is required." }),
+    password: z.string().min(1, { message: "Must enter a password." })
+})
