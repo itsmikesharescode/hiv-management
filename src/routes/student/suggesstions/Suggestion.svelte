63e4cf0f021193ba4bb66cee169405fd37c1b533
itsmikesharescode <mikejohneviota@gmail.com>
2024-05-02 18:41:43 +0800

    add animation tracking

--- src/routes/student/suggesstions/Suggestion.svelte
+++ src/routes/student/suggesstions/Suggestion.svelte
@@ -1,19 +1,45 @@
 <script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
 	import * as Card from '$lib/components/ui/card';
+	import { flip } from 'svelte/animate';
 	import { suggestions } from './generatedtext';
+	import { RefreshCw } from 'lucide-svelte';
+
+	let randomIndex = 0;
+	let loader = false;
+	const generateSuggestions = () => {
+		animateLoading();
+		let newIndex = 0;
+		do {
+			newIndex = Math.round(Math.random() * (suggestions.length - 1));
+		} while (newIndex === randomIndex);
+		randomIndex = newIndex;
+	};
+
+	const animateLoading = () => {
+		loader = true;
+		setTimeout(() => (loader = false), 800);
+	};
 </script>
 
+<div class="flex justify-end">
+	<Button on:click={generateSuggestions} class="flex items-center gap-[10px]"
+		>More Suggestions <RefreshCw class={loader ? 'animate-spin' : ''} /></Button
+	>
+</div>
 <div class="grid grid-cols-1 gap-[10px] md:grid-cols-2 lg:grid-cols-3">
-	{#each suggestions[Math.round(Math.random() * (suggestions.length - 1))] as suggestion}
-		<Card.Root>
-			<Card.Header>
-				<Card.Title>{suggestion.title}</Card.Title>
-			</Card.Header>
-			<Card.Content>
-				<p>
-					{suggestion.definition}
-				</p>
-			</Card.Content>
-		</Card.Root>
+	{#each suggestions[randomIndex] as suggestion (suggestion.id)}
+		<div class="grid" animate:flip={{ duration: 920 }}>
+			<Card.Root>
+				<Card.Header>
+					<Card.Title>{suggestion.title}</Card.Title>
+				</Card.Header>
+				<Card.Content>
+					<p>
+						{suggestion.definition}
+					</p>
+				</Card.Content>
+			</Card.Root>
+		</div>
 	{/each}
 </div>
--- src/routes/student/suggesstions/generatedtext.ts
+++ src/routes/student/suggesstions/generatedtext.ts
@@ -1,38 +1,47 @@
 
 const suggestion1 = [
     {
+        id: 0,
         title: "Practice Safe Sex",
         definition: "Use condoms and dental dams consistently and correctly to prevent the exchange of bodily fluids, which can significantly reduce the risk of HIV transmission during vaginal, anal, and oral sex."
     },
     {
+        id: 1,
         title: "Get Tested Regularly",
         definition: "Know your HIV status and that of your sexual partners, as undiagnosed infection can lead to unintended transmission and delayed treatment."
     },
     {
+        id: 2,
         title: "Use Pre-Exposure Prophylaxis (PrEP)",
         definition: "Take daily medication to reduce the risk of HIV infection by up to 99% in individuals at high risk, such as those with HIV-positive partners or engaging in high-risk sexual behavior."
     },
     {
+        id: 3,
         title: "Use Post-Exposure Prophylaxis (PEP)",
         definition: "Take medication within 72 hours of potential exposure to prevent infection, which can be effective in emergency situations such as sexual assault or needlestick injuries."
     },
     {
+        id: 4,
         title: "Avoid Sharing Needles",
         definition: "Use clean needles and syringes for injections and tattoos to prevent the transmission of HIV and other bloodborne pathogens."
     },
     {
+        id: 5,
         title: "Avoid Mother-to-Child Transmission",
         definition: "If you're pregnant or breastfeeding, get tested and treated if necessary to prevent passing HIV to your child."
     },
     {
+        id: 6,
         title: "Practice Monogamy",
         definition: "Mutually faithful relationships reduce the risk of HIV transmission, as both partners are committed to only having sex with each other."
     },
     {
+        id: 7,
         title: "Get Vaccinated",
         definition: "Consider getting vaccinated against hepatitis B, which shares similar transmission routes and can increase the risk of HIV infection."
     },
     {
+        id: 8,
         title: "Stay Informed",
         definition: "Educate yourself about HIV risks, symptoms, and prevention methods to make informed decisions about your sexual health and take control of your well-being."
     }
@@ -40,34 +49,42 @@ const suggestion1 = [
 
 const suggestion2 = [
     {
+        id: 1,
         title: "Know Your Status",
         definition: "Get tested for HIV regularly to know your status and take control of your health, as early detection and treatment can significantly improve quality of life and prevent transmission to others."
     },
     {
+        id: 2,
         title: "Protect Your Partner",
         definition: "Use condoms and PrEP consistently and correctly to prevent transmitting HIV to your sexual partners, as undiagnosed infection can lead to unintended transmission and delayed treatment."
     },
     {
+        id: 3,
         title: "Safe Blood, Safe Life",
         definition: "Ensure all blood transfusions and organ donations are screened for HIV to prevent transmission, as contaminated blood products can pose a significant risk of infection to recipients."
     },
     {
+        id: 4,
         title: "Clean Needles Save Lives",
         definition: "Use sterile needles and syringes for injections and tattoos to prevent HIV transmission, as sharing contaminated needles can spread the virus quickly among individuals and communities."
     },
     {
+        id: 5,
         title: "Healthy Moms, Healthy Babies",
         definition: "Pregnant women with HIV can take treatment to prevent passing it to their babies, as early intervention and proper care can significantly reduce the risk of mother-to-child transmission."
     },
     {
+        id: 6,
         title: "Monogamy Matters",
         definition: "Mutually faithful relationships reduce the risk of HIV transmission and other STIs, as both partners are committed to only having sex with each other and minimizing exposure to potential infections."
     },
     {
+        id: 7,
         title: "Get Vaccinated, Stay Protected",
         definition: "Vaccines like hepatitis B can reduce the risk of HIV transmission by preventing co-infections, as a healthy immune system is better equipped to fight off potential infections and diseases."
     },
     {
+        id: 8,
         title: "Safe Sex, Every Time",
         definition: "Use condoms and dental dams consistently and correctly to prevent HIV transmission during sex, as unprotected sex is a significant risk factor for contracting the virus and other STIs."
     },
@@ -79,34 +96,42 @@ const suggestion2 = [
 
 const suggestion3 = [
     {
+        id: 1,
         title: "Get Tested Together",
         definition: "Get tested for HIV with your sexual partner to know your status, build trust, and take control of your health, as mutual knowledge and support can strengthen your relationship and prevent transmission."
     },
     {
+        id: 2,
         title: "Use Condoms Correctly",
         definition: "Use condoms consistently and correctly to prevent HIV transmission during sex, as proper use can significantly reduce the risk of infection and other STIs, and ensure a healthy and safe sexual experience."
     },
     {
+        id: 3,
         title: "Prevent Mother-to-Child Transmission",
         definition: "Pregnant women with HIV can take treatment to prevent passing it to their babies, as early intervention and proper care can significantly reduce the risk of mother-to-child transmission and ensure a healthy start for the newborn."
     },
     {
+        id: 4,
         title: "Practice Safe Injection",
         definition: "Use sterile needles and syringes for injections and tattoos to prevent HIV transmission, as sharing contaminated needles can spread the virus quickly among individuals and communities, and have devastating consequences."
     },
     {
+        id: 5,
         title: "Get Educated, Get Empowered",
         definition: "Educate yourself about HIV risks, symptoms, and prevention methods to make informed decisions about your sexual health, as knowledge is power in the fight against HIV and other STIs, and can help you take control of your wellbeing and live a healthy life."
     },
     {
+        id: 6,
         title: "Support HIV Research",
         definition: "Support organizations and research initiatives working towards an HIV cure and improved treatment options, as funding and awareness can accelerate progress and bring us closer to a world without HIV and AIDS."
     },
     {
+        id: 7,
         title: "Use Pre-Exposure Prophylaxis (PrEP)",
         definition: "Take daily medication to reduce the risk of HIV infection by up to 99% in individuals at high risk, such as those with HIV-positive partners or engaging in high-risk sexual behavior, as PrEP can provide a safe and effective barrier against infection."
     },
     {
+        id: 8,
         title: "Stay Faithful, Stay Safe",
         definition: "Mutually faithful relationships reduce the risk of HIV transmission and other STIs, as both partners are committed to only having sex with each other and minimizing exposure to potential infections, and can build trust and strengthen the bond between partners."
     },
@@ -118,38 +143,47 @@ const suggestion3 = [
 
 const suggestion4 = [
     {
+        id: 0,
         title: "Know Your Risk Factors",
         definition: "Understand your risk factors for HIV transmission, such as unprotected sex, sharing needles, and mother-to-child transmission, to take informed precautions and prevent infection, as knowledge is power in the fight against HIV and AIDS."
     },
     {
+        id: 1,
         title: "Use Condoms and Lubricants",
         definition: "Use condoms and lubricants consistently and correctly to prevent HIV transmission during sex, as proper use can significantly reduce the risk of infection and other STIs, and ensure a healthy and safe sexual experience, while also preventing unwanted pregnancies and STIs."
     },
     {
+        id: 2,
         title: "Get Tested for STIs",
         definition: "Get tested for STIs, including HIV, regularly to know your status and take control of your health, as undiagnosed infection can lead to unintended transmission and delayed treatment, and early detection can prevent long-term health consequences and complications."
     },
     {
+        id: 3,
         title: "Avoid Sharing Needles",
         definition: "Avoid sharing needles and syringes for injections and tattoos to prevent HIV transmission, as sharing contaminated needles can spread the virus quickly among individuals and communities, and have devastating consequences, including infection and death."
     },
     {
+        id: 4,
         title: "Practice Safe Sex Communication",
         definition: "Communicate openly and honestly with your sexual partners about your HIV status, sexual history, and safe sex practices to ensure mutual understanding and protection, and build trust and respect in your relationships, while also preventing unintended transmission and promoting healthy sexual experiences."
     },
     {
+        id: 5,
         title: "Support HIV-Positive Individuals",
         definition: "Support individuals living with HIV and AIDS, and promote awareness and acceptance to reduce stigma and discrimination, as a supportive community can improve mental and physical health outcomes, and help individuals feel empowered and valued members of society."
     },
     {
+        id: 6,
         title: "Use Post-Exposure Prophylaxis (PEP)",
         definition: "Use PEP medication within 72 hours of potential exposure to prevent HIV infection, as timely intervention can significantly reduce the risk of transmission and provide peace of mind, and should be used in conjunction with other prevention methods for optimal protection."
     },
     {
+        id: 7,
         title: "Stay Informed About HIV",
         definition: "Stay informed about HIV risks, symptoms, and prevention methods to make informed decisions about your sexual health, as knowledge is power in the fight against HIV and AIDS, and can help you take control of your wellbeing and live a healthy life, free from the threat of HIV and other STIs."
     },
     {
+        id: 8,
         title: "Get Involved in HIV Awareness",
         definition: "Get involved in HIV awareness campaigns and events to promote education, support, and advocacy, as a united community can raise awareness, reduce stigma, and promote prevention and treatment, and help create a world without HIV and AIDS."
     },
